056cd5e5e093dcb2100a37581d2883fddd0ce6a5
AlexeiMir <alex_16.07@mail.ru>
2019-11-29 17:20:42 +0700

    23 lesson work

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import s from './Profile.module.css';
-import MyPosts from './MyPosts/MyPosts'
+import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+
 
 const Profile = () => {
-  return <div>
-    <img src="http://www.imgworlds.com/wp-content/uploads/2015/12/generic.jpg"></img>
-
-    <div>
-      Ava+description
-  </div>
-  <MyPosts />
-  </div>
+  return (
+  <div>
+    <ProfileInfo />
+    <MyPosts />
+    </div>
+    ) 
   
 }
 
