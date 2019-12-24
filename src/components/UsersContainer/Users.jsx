35b338d10a67483aea855537248f9a3410e5cf9f
AlexeiMir <alex_16.07@mail.ru>
2019-12-24 08:35:44 +0700

    51 lesson

--- src/components/UsersContainer/Users.jsx
+++ src/components/UsersContainer/Users.jsx
@@ -1,46 +1,24 @@
 import React from 'react';
 import style from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from './../../assets/images/user.png'
 
 
 
 let Users = (props) => {
    
     if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+         
+        props.setUsers(response.data.items)});
        
-        props.setUsers(
-            [{
-                id: 1,
-                photoUrl: 'https://i08.fotocdn.net/s113/0caef68e3cadf202/user_m/2560307574.jpg',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: {city:'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl:'https://i08.fotocdn.net/s113/0caef68e3cadf202/user_m/2560307574.jpg',
-                followed: true,
-                fullName: 'Anton',
-                status: 'I am a boss too',
-                location: {city:'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl:'https://i08.fotocdn.net/s113/0caef68e3cadf202/user_m/2560307574.jpg',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am a boss too',
-                location: {city:'Kiev', country: 'Ukraine'}
-            }
-            ]
-            )
     }
 
 
     return <div> {props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={style.userPhoto}/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto } className={style.userPhoto}/>
                 </div>
                 <div>
                     {u.followed ? 
@@ -50,12 +28,12 @@ let Users = (props) => {
             </span>
             <span>
                 <span>
-                    <div> {u.fullName} </div>
+                    <div> {u.name} </div>
                     <div> {u.status} </div>
                 </span>
                 <span>
-                    <div> {u.location.country} </div>
-                    <div> {u.location.city} </div>
+                    <div> "u.location.country" </div>
+                    <div> "u.location.city" </div>
                 </span>
             </span>
         </div>
