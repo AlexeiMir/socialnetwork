a151a28238a73781a087f54e68a8dad92b681bf5
AlexeiMir <alex_16.07@mail.ru>
2020-01-07 21:58:43 +0700

    67 lesson

--- src/api/api.js
+++ src/api/api.js
@@ -24,6 +24,16 @@ export const usersAPI = {
         return instance.delete(`follow/${userId}`).then(response => {
             return response.data
         })
+    },
+
+    getProfile(userId) {
+        return instance.get(`profile/`+userId)
+    }
+}
+
+export const authAPI = {
+    me() {
+        return instance.get(`auth/me`)
     }
 }
 
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import Header from './Header';
 import { connect } from 'react-redux';
-import {setAuthUserData} from '../../redux/auth-reducer';
-import * as axios from 'axios';
+import {getAuthUserData} from '../../redux/auth-reducer';
+
 
 
 
 class HeaderContainer extends React.Component {
 
     componentDidMount() {
-      
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me
-        `,{ withCredentials: true}).then(response => {
+
+        this.props.getAuthUserData();
+
+
+        /*usersAPI.AuthUserData().then(response => {
             if ((response.data.resultCode) === 0) {
                 let {id, login, email} = response.data.data;
                 
             this.props.setAuthUserData(id, login, email)
            
         }
-        }); 
+        }); */
 
     } 
 
@@ -39,4 +41,4 @@ let mapStateToProps =(state) => {
     
 }
 
-export default connect(mapStateToProps, {setAuthUserData}) (HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData}) (HeaderContainer);
