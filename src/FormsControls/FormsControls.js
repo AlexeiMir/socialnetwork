d36294612c9e0e4da130b73842e755d0f24cd10c
AlexeiMir <alex_16.07@mail.ru>
2020-01-17 22:38:47 +0700

    77 lesson 2

--- src/FormsControls/FormsControls.js
+++ src/FormsControls/FormsControls.js
@@ -3,13 +3,23 @@ import styles from './FormsControls.module.css'
 
 
 
-export const Textarea = ({input,meta, ...restProps}) => {
+export const FormControl = ({input,meta,child, ...props}) => {
     const hasErrors = meta.error&&meta.touched
     return <div className={styles.formControl+" "+(hasErrors?styles.error:'')}>
     <div>
-    <textarea {...input} {...restProps}/>
+    {props.children}
     </div>
    {hasErrors&&<span>{meta.error}</span>}
     </div> 
 
 }
+
+export const Input = (props) => {
+   const {input,meta,child, ...restProps} = props;
+   return <FormControl {...props}> <input {...input} {...restProps} /> </FormControl>
+}
+
+export const Textarea = (props) => {
+    const {input,meta,child, ...restProps} = props;
+    return <FormControl {...props}> <textarea {...input} {...restProps} /> </FormControl>
+ }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,18 @@
 import React from "react"
 import { reduxForm, Field } from "redux-form"
+import {Input} from '../../FormsControls/FormsControls'
+import { required,maxLengthCreator } from "../../utils/validators/validators"
 
+const maxLenght20 = maxLengthCreator(20)
 
 const LoginPage = (props) => {
     
     return (
     <form onSubmit={props.handleSubmit}>
      <div>
-        <div> <Field placeholder={"Login"} name={"login"} component={"input"} /> </div>
-        <div> <Field placeholder={"Password"} name={"password"} component={"input"} /> </div>
-        <div><Field type={"checkbox"} name={"rememberMe"} component={"input"}/>remember me</div>
+        <div> <Field placeholder={"Login"} name={"login"} component={Input} validate={[required,maxLenght20]} /> </div>
+        <div> <Field placeholder={"Password"} name={"password"} component={Input} /> </div>
+        <div><Field type={"checkbox"} name={"rememberMe"} component={Input}/>remember me</div>
         <div> <button>Login</button>  </div>
     </div>
     </form>
