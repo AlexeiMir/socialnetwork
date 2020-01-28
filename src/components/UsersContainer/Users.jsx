1c8211cd37801bebabdde62439769c9264f96f4d
AlexeiMir <alex_16.07@mail.ru>
2020-01-28 15:53:51 +0700

    90 lesson 3

--- src/components/UsersContainer/Users.jsx
+++ src/components/UsersContainer/Users.jsx
@@ -8,25 +8,11 @@ import Paginator from '../../utils/Paginator/Paginator'
 
 
 
-let Users = (props) => {
-
-
-       let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
-       let pages = [];
-       for (let i=1;i<=pagesCount;i++) {
-            pages.push(i);
-       }
-    
-
-
+let Users = (totalUsersCount,pageSize,currentPage,onPageChanged, ...props) => {
         return <div>
-            <div>
-                {pages.map(p =>{
-                return <span className={(props.currentPage === p) && style.selectedPage} onClick = {
-                    (e) =>{props.onPageChanged(p)}}>{p}</span>})}
-            </div>
+        
+            <Paginator totalUsersCount={totalUsersCount} pageSize={pageSize} currentPage={currentPage} onPageChanged={onPageChanged}/>
 
-            <Paginator />
         {props.users.map(u => <div key={u.id}>
             <span>
                 <NavLink to={'/profile/'+ u.id}>
--- src/utils/Paginator/Paginator.jsx
+++ src/utils/Paginator/Paginator.jsx
@@ -3,7 +3,7 @@ import styles from './Paginator.module.css'
 
 const Paginator = (totalUsersCount,pageSize,currentPage,onPageChanged) => {
 
-let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
+let pagesCount = Math.ceil(totalUsersCount/pageSize);
 let pages = [];
 for (let i=1;i<=pagesCount;i++) {
      pages.push(i);
@@ -14,8 +14,8 @@ for (let i=1;i<=pagesCount;i++) {
  return <div>
      
          {pages.map(p =>{
-         return <span className={(props.currentPage === p) && styles.selectedPage} onClick = {
-             (e) =>{props.onPageChanged(p)}}>{p}</span>})}
+         return <span className={(currentPage === p) && styles.selectedPage} onClick = {
+             (e) =>{onPageChanged(p)}}>{p}</span>})}
  </div>
      }
 
