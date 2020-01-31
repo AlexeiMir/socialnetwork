d76d6dec04f4bcae268b685fe8eeefa120de1987
AlexeiMir <alex_16.07@mail.ru>
2020-01-31 22:45:37 +0700

    94 lesson

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import {Route, withRouter,BrowserRouter } from 'react-router-dom';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import News from './components/News/News';
 import Music from './components/Music/Music';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/UsersContainer/UsersContainer';
 import Login from './components/Login/Login';
 import {connect} from 'react-redux'
@@ -15,6 +13,10 @@ import {initializeApp} from './redux/app-reducer'
 import Preloader from './components/common/Preloader/Preloader';
 import {Provider} from 'react-redux';
 import store from './redux/redux-store'
+import {withSuspense} from './hoc/withSuspense'
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
 
@@ -32,8 +34,8 @@ class App extends Component {
       <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
-  <Route path='/profile/:userId?' render={ () =><ProfileContainer />} /> 
-      <Route path='/dialogs' render={() =><DialogsContainer />}/>
+  <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} /> 
+      <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
       <Route path='/users' render={() =><UsersContainer />}/>
       <Route path='/news'component={News} />
       <Route path='/music'component={Music} />
