39d86c098c7c2e0f609af55f306bfeef66e76793
AlexeiMir <alex_16.07@mail.ru>
2020-03-18 13:59:37 +0700

    todo 2

--- src/components/Todolist/Todolist.jsx
+++ src/components/Todolist/Todolist.jsx
@@ -1,50 +1,70 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import s from './Todolist.module.css';
-import { Row, Col } from 'antd';
+import {Row, Col} from 'antd';
 
 
 class Todolist extends React.Component {
-
-    state = {
-        editeMode: false,
-        tasks: ['learn js', 'learn react']
+    constructor(props) {
+        super();
+        this.state = {
+            tasks: [
+                {
+                    isDone: false,
+                    title: 'learn js'
+                },
+                {
+                    isDone: false,
+                    title: 'learn react'
+                }
+            ]
+        }
     }
 
-    addTask = (e) =>{
-        this.setState({task:e.currentTarget.value})
+
+    createNewTask = (e) => {
+        if (e.key === 'Enter') {
+            this.setState({tasks: [...this.state.tasks, {title:e.currentTarget.value,isDone: false}]})
+            e.currentTarget.value = ''
+        }
+
 
     }
 
-    toggleTaskStatus(){
+    toggleTaskStatus() {
 
     }
 
-    deleteTask() {
-        this.setState(...this.state, this.state.tasks.filter())
+    deleteTask(task, e) {
+        this.setState({tasks: this.state.tasks.filter((t) => {
+            return t!== task
+        })})
     }
 
     render() {
 
-        return <Row>
+        return <Row className={s.toDoList}>
             <Row>
-                <Col span={8} >
-                    <input onChange={this.addTask.bind(this)} value = {this.state.task}/>
+                <Col span={8}>
+                    <input onKeyPress={this.createNewTask.bind(this)}/>
                 </Col>
             </Row>
             <Row>
-                <Col span={8}>
-    {this.state.tasks.map((task)=>{ return <Row>
-        <Col span={6}><input type="checkbox" onChange={this.toggleTaskStatus.bind(this)}/></Col>
-        <Col span={12}>{task}
-    </Col>
-    <Col span={6} className={s.delete} onChange={this.deleteTask.bind(this,task)}></Col>
-    </Row>})
-    
-    }
+                <Col span={8} className={s.task}>
+                    {this.state.tasks.map((task) => {
+                        return <Row>
+                            <Col span={6}><input type="checkbox" onChange={this.toggleTaskStatus.bind(this)}/></Col>
+                            <Col span={12}>{task.title}
+                            </Col>
+                            <Col span={6} className={s.delete} onChange={this.deleteTask.bind(this, task)}>X</Col>
+                        </Row>
+                    })
+
+                    }
                 </Col>
             </Row>
         </Row>
     }
 }
+
 export default Todolist;
